0ab25019794a116e751b75df6601d6e6454cad58
Alberto <43555310+T0kiii@users.noreply.github.com>
2021-02-06 19:28:06 +0100

    Primer commit.

--- app/src/main/java/com/example/hepal/MainActivity.java
+++ app/src/main/java/com/example/hepal/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Alberto
+ * @author Héctor Luis
+ * @author Pablo Blesa
+ *
+ * Aplicación para la empresa HEPAL
+ *
+ * @version 06-02-2021*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
